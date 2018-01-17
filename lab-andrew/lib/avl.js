2d3e07b0ad92014256b009a77e63d4fbc47447e4
Andrew Bloom <blooma116@gmail.com>
2018-01-16 16:07:39 -0800

    working AVL remove method, tree does not retain balance after remove currently

--- lab-andrew/lib/avl.js
+++ lab-andrew/lib/avl.js
@@ -31,6 +31,33 @@ class AVL{
     return this._find(node.right, value);
   }
 
+  _preDelete(node, value){
+    if (!node){
+      return;
+    }
+    if (node.data === value){
+      return;
+    }
+    if (node.left){
+      if (node.left.data === value){
+        node.balance++;
+        return;
+      }
+    }
+    if (node.right){
+      if (node.right.data === value){
+        node.balance--;
+        return;
+      }
+    }
+    if (node.data > value){
+      node.balance++;
+      return this._preDelete(node.left, value);
+    }
+    node.balance--;
+    return this._preDelete(node.right, value);
+  }
+
   insert(value){
     if (!this.root) {
       this.root = new AVLNode(value);
@@ -40,7 +67,11 @@ class AVL{
       return -1;
     }
     this._insert(this.root, value);
-    if (this.root.balance < -1){
+    this._rootRotate();
+  }
+
+  _rootRotate(){
+    if (this.root.balance < -1) {
       if (this.root.left.balance === -1) {
         this.root = this._rotateLL(this.root);
         this._setBalance(this.root);
@@ -52,7 +83,7 @@ class AVL{
         return;
       }
     }
-    if (this.root.balance > 1){
+    if (this.root.balance > 1) {
       if (this.root.right.balance === -1) {
         this.root = this._rotateRL(this.root);
         this._setBalance(this.root);
@@ -125,12 +156,12 @@ class AVL{
           node.left.balance--;
           return;
         }
-        if (node.left.right.balance < 0){
-          node.left = this._rotateRL(node.left);
+        if (node.left.right.balance > 0){
+          node.left = this._rotateRR(node.left);
           this._setBalance(node.right);
           return;
         }
-        node.left = this._rotateRR(node.left);
+        node.left = this._rotateRL(node.left);
         this._setBalance(node.right);
         return;
       }
@@ -157,12 +188,12 @@ class AVL{
           node.right.balance--;
           return;
         }
-        if (node.right.right.balance < 0) {
-          node.right = this._rotateRL(node.right);
+        if (node.right.right.balance > 0) {
+          node.right = this._rotateRR(node.right);
           this._setBalance(node.right);
           return;
         }
-        node.right = this._rotateRR(node.right);
+        node.right = this._rotateRL(node.right);
         this._setBalance(node.right);
         return;
       }
@@ -215,6 +246,105 @@ class AVL{
     } else {
       return false;
     }
+  }
+
+  remove(value){
+    let node = this.find(value);
+    if (node === -1){
+      return -1;
+    }
+
+    if (this.root.data > value){
+      this.root.balance++;
+    } else {
+      this.root.balance--;
+    }
+    this._preDelete(this.root, value);
+
+    if (node.left && node.right){
+      node.data = this._findMinValue(node.right);
+      if (node.data === node.right.data){
+        if (!node.right.right){
+          node.right = null;
+          this._setBalance(node);
+          this._rootRotate();
+          return;
+        }
+        node.right = node.right.right;
+        this._setBalance(node);
+        this._rootRotate();
+        return;
+      }
+      this._remove(node.right, node.data);
+      this._setBalance(node);
+      this._rootRotate();
+      return; 
+    }
+    if ((node.left && !node.right) || (!node.left && node.right)){
+      if (node === this.root){
+        if (this.root.left){
+          this.root = this.root.left;
+          this._setBalance(this.root);
+          this._rootRotate();
+          return;
+        }
+        this.root = this.root.right;
+        this._setBalance(this.root);
+        this._rootRotate();
+        return;
+      }
+      this._remove(this.root, value);
+      this._setBalance(node);
+      this._rootRotate();
+      return;
+    }
+    if (node === this.root){
+      this.root = null;
+      return;
+    }
+    this._remove(this.root, value);
+    this._setBalance(node);
+    this._rootRotate();
+    return;
+  }
+
+  _findMinValue(node){
+    if (node.left){
+      return this._findMinValue(node.left);
+    }
+    return node.data;
+  }
+
+  _remove(node, value){
+    if (value > node.data) {
+      if (node.right.data === value) {
+        if (!node.right.left && node.right.right) {
+          node.right = node.right.right;
+          return;
+        }
+        if (node.right.left && !node.right.right) {
+          node.right = node.right.left;
+          return;
+        }
+        node.right = null;
+        return;
+      }
+      return this._remove(node.right, value);
+    }
+
+    if (node.left.data === value) {
+      if (!node.left.left && node.left.right) {
+        node.left = node.left.right;
+        return;
+      }
+      if (node.left.left && !node.left.right) {
+        node.left = node.left.left;
+        return;
+      }
+      node.left = null;
+      return;
+    }
+    return this._remove(node.left, value);
   }
 
 }
